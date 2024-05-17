4c53cb896129835bbd041632b6de14a8937e6c83
priyank rai <type2email@gmail.com>
2024-05-17 16:22:04 +0530

    bugfix

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -7,47 +7,62 @@ const Button = ({ item, buttonType }) => {
   const [isPriceAvailabele, setIsPriceAvailable] = useState(false);
   const [buttonText, setButtonText] = useState(item);
   const [priceList, setPriceList] = useState(null);
-  const { selectedData, setSelectedData, saltFormsJson } = useContext(ChoiceContext);
+  const { selectedData, setSelectedData, saltFormsJson } =
+    useContext(ChoiceContext);
 
   useEffect(() => {
-    if (!(selectedData.form && selectedData.strength && selectedData.packing)) return;
+    if (!selectedData.form || !selectedData.strength || !selectedData.packing)
+      return;
 
     let priceList = [];
     if (buttonType === "strength") {
-      if (saltFormsJson[selectedData.form] && saltFormsJson[selectedData.form][buttonText]) {
-        priceList = saltFormsJson[selectedData.form][buttonText][selectedData.packing];
+      if (
+        saltFormsJson[selectedData.form] &&
+        saltFormsJson[selectedData.form][buttonText]
+      ) {
+        priceList =
+          saltFormsJson[selectedData.form][buttonText][selectedData.packing];
       }
     } else if (buttonType === "form") {
-      if (saltFormsJson[buttonText] && saltFormsJson[buttonText][selectedData.strength]) {
-        priceList = saltFormsJson[buttonText][selectedData.strength][selectedData.packing];
+      if (
+        saltFormsJson[buttonText] &&
+        saltFormsJson[buttonText][selectedData.strength]
+      ) {
+        priceList =
+          saltFormsJson[buttonText][selectedData.strength][
+            selectedData.packing
+          ];
       }
     } else if (buttonType === "packing") {
-      if (saltFormsJson[selectedData.form] && saltFormsJson[selectedData.form][selectedData.strength]) {
-        priceList = saltFormsJson[selectedData.form][selectedData.strength][item];
+      if (
+        saltFormsJson[selectedData.form] &&
+        saltFormsJson[selectedData.form][selectedData.strength]
+      ) {
+        priceList =
+          saltFormsJson[selectedData.form][selectedData.strength][item];
       }
     }
 
-    if (priceList) {
-      setPriceList(priceList);
-    }
-  }, [selectedData, item, saltFormsJson, buttonType, buttonText]);
-
-  useEffect(() => {
     if (priceList) {
       Object.keys(priceList).forEach((key) => {
         if (priceList[key]) {
           setIsPriceAvailable(true);
+          return;
         }
       });
     }
-  }, [priceList]);
+  }, [selectedData, item, saltFormsJson, buttonType, buttonText]);
 
   return (
     <li
       className={`
         p-1 rounded-md border-2 backdrop-blur-md 
         ${isPriceAvailabele ? "border-solid" : "border-dashed"} 
-        ${item === selectedData[buttonType] ? "border-blue-400" : "border-gray-400"}
+        ${
+          item === selectedData[buttonType]
+            ? "border-blue-400"
+            : "border-gray-400"
+        }
       `}
       onClick={() => setSelectedData({ ...selectedData, [buttonType]: item })}
     >
